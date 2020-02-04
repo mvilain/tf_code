ff70ccf33d50cfe10ba6081c2dfaa6b7278764cb
Michael Vilain <michael@vilain.com>
2020-02-04 13:04:18 -0800

    add variables, load balancer and autoscaling

--- prod.tf
+++ prod.tf
@@ -1,8 +1,38 @@
-provider "aws" {
-  profile = "default"
-  region  = "us-east-2"
+# ================================================== VARIABLES
+variable "region" {
+  type    = string
+  default = "us-east-2"
 }
 
+variable "az_subnets" {
+  type    = list(string)
+  default = [
+    "us-east-2a",
+    "us-east-2b",
+    "us-east-2c",
+  ]
+}
+
+# Bitnami nginx Open Source Cert in us-east-2
+variable "nginx_ami" {
+  type        = string
+  description = "region (AMI) to use for nginx"
+  default     = "ami-06249d482a680ae8d"
+  
+#  validation {
+#    condition     = length(var.nginx_ami) > 4 && substr(var.nginx_ami, 0, 4) == "ami-"
+#    error_message = "Machine Image must be a valid AMI id, starting with \"ami-\"."
+#  }
+}
+
+######################################################################
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+# ================================================== S3
 resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "mvilain-prod-tf-course-20200203"
   acl    = "private"
@@ -15,31 +45,30 @@ resource "aws_default_vpc" "default" {
   }
 }
 
-resource "aws_default_subnet" "default_azA" {
-  availability_zone = "us-east-2a"
+resource "aws_default_subnet" "default_az0" {
+  availability_zone = var.az_subnets.0
   
     tags = {
     "Terraform" : "true"
-    "Name"      : "prod_web_azA"
+    "Name"      : "prod_web_az0"
   }
 }
-resource "aws_default_subnet" "default_azB" {
-  availability_zone = "us-east-2b"
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = var.az_subnets.1
   
     tags = {
     "Terraform" : "true"
-    "Name"      : "prod_web_azB"
+    "Name"      : "prod_web_az1"
   }
 }
-resource "aws_default_subnet" "default_azC" {
-  availability_zone = "us-east-2c"
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = var.az_subnets.2
   
     tags = {
     "Terraform" : "true"
-    "Name"      : "prod_web_azC"
+    "Name"      : "prod_web_az2"
   }
 }
-
 
 # ================================================== SECURITY GROUPS
 resource "aws_security_group" "prod_web" {
@@ -52,7 +81,7 @@ resource "aws_security_group" "prod_web" {
     protocol    = "tcp"
     cidr_blocks = [
       "0.0.0.0/0",
-      "1.2.3.4/32"
+#      "1.2.3.4/32"
     ]
   }
   ingress {
@@ -61,7 +90,7 @@ resource "aws_security_group" "prod_web" {
     protocol    = "tcp"
     cidr_blocks = [
       "0.0.0.0/0",
-      "1.2.3.4/32"
+#      "1.2.3.4/32"
     ]
   }
 
@@ -71,7 +100,7 @@ resource "aws_security_group" "prod_web" {
     protocol    = "-1"
     cidr_blocks = [
       "0.0.0.0/0",
-      "1.2.3.4/32"
+#      "1.2.3.4/32"
     ]
   }
 
@@ -82,10 +111,9 @@ resource "aws_security_group" "prod_web" {
 }
 
 # ================================================== INSTANCES
-# Bitnami NGINX Open Source Cert in us-east-2
 resource "aws_instance" "prod_web" {
-  count         = 2
-  ami           = "ami-06249d482a680ae8d"
+  count         = 3
+  ami           = var.nginx_ami
   instance_type = "t2.nano"
 
 
@@ -114,11 +142,12 @@ resource "aws_eip" "prod_web" {
 resource "aws_elb" "prod_web"{
   name            = "prod-web-lb"
   instances       = aws_instance.prod_web.*.id
-  subnets         = [ 
-    aws_default_subnet.default_azA.id,
-    aws_default_subnet.default_azB.id, 
-    aws_default_subnet.default_azC.id
-    ]
+  subnets         = var.az_subnets.*
+#  depends_on      = [ 
+#    aws_default_subnet.default_az0,
+#    aws_default_subnet.default_az1,
+#    aws_default_subnet.default_az1
+#    ]
   security_groups = [aws_security_group.prod_web.id]
 
   listener {
@@ -132,4 +161,36 @@ resource "aws_elb" "prod_web"{
     "Terraform" : "true"
     "Name"      : "prod_web"
   }
+}
+
+# ================================================== AUTOSCALING
+resource "aws_autoscaling_group" "prod_web" {
+  name                 = "prod_web_asg"
+  desired_capacity     = 1
+  health_check_type    = "ELB"
+  launch_configuration = aws_launch_configuration.prod_web.id
+  max_size             = 2
+  min_size             = 1
+  availability_zones   = var.az_subnets.*
+#  availability_zones   = [
+#    aws_default_subnet.default_azA.id,
+#    aws_default_subnet.default_azB.id, 
+#    aws_default_subnet.default_azC.id
+#  ]
+
+}
+
+resource "aws_launch_configuration" "prod_web" {
+  name              = "prod_web_lc"
+  image_id          = var.nginx_ami
+  instance_type     = "t2.nano"
+
+  security_groups   = [
+    aws_security_group.prod_web.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+  }
+
+  
 }
