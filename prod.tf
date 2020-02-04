12eae94741cc1bdea94ec3d1d2d2e0ece8b2d72c
Michael Vilain <michael@vilain.com>
2020-02-03 21:34:27 -0800

    defined init web security group

--- prod.tf
+++ prod.tf
@@ -13,3 +13,41 @@ resource "aws_default_vpc" "default" {
     Name = "Default VPC"
   }
 }
+
+resource "aws_security_group" "prod_web" {
+  name  = "prod_web"
+  description = "Allow standard http+https ports inbound, all outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0",
+      "1.2.3.4/32"
+    ]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0",
+      "1.2.3.4/32"
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0",
+      "1.2.3.4/32"
+    ]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
